e84a517e8a0e5cd5895e87b93ca82aa7b821f197
tkushch <73709700+tkushch@users.noreply.github.com>
2020-11-02 12:38:06 +0300

    Update TV.java

--- app/src/main/java/com/example/emptyexample/TV.java
+++ app/src/main/java/com/example/emptyexample/TV.java
@@ -1,11 +1,24 @@
 package com.example.emptyexample;
-// new
-public class TV {
+
+public class Board {
     int a = 0;
     int b = 0;
-    // comment
-    public TV(){
+    String messages = '';
+    public Board(){
         this.a = 1;
         this.b = 1;
     }
+    public void sendtoBoard(TV tv1, m){
+        tv1.messages += (m + "\n");
+    }
+    public void sendtoPhone(Phone ph1, m){
+        ph1.messages += (m + "\n");
+    }
+    public String showMessages(){
+        return this.messages;
+    }
+    public void clearMessages(){
+        this.messages = "";
+    }
+    
 }
